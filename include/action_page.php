33eb0c7dfb711c056d063c1c65bf60958b66a348
William Nash <wrnash1@gmail.com>
2021-01-21 14:59:28 -0600

    bug

--- include/action_page.php
+++ include/action_page.php
@@ -5,8 +5,8 @@ require_once "Database.php";
 $db = new Database("localhost", "eyh", "horizon", "EYH");
 
 /* Query */
-$sql = "INSERT INTO student (name, address, city, state, zip, email, grade, size, attend, troop, school, teacher, topic1, topic2)
-VALUES (:name, :address, :city, :state, :zip, :email, :grade, :size, :attend, :troop, :school, :teacher, :topic1, :topic2)";
+$sql = "INSERT INTO student (name, address, city, state, zip, teacher, email, grade, attend, troop, size, school, topic1, topic2)
+VALUES (:name, :address, :city, :state, :zip, :teacher, :email, :grade, :attend, :troop, :size,  :school, :topic1, :topic2)";
 
 /* Initiate query */
 $db->query($sql);
@@ -17,13 +17,13 @@ $db->bind(':address', $_REQUEST['address']);
 $db->bind(':city', $_REQUEST['city']);
 $db->bind(':state', $_REQUEST['state']);
 $db->bind(':zip', $_REQUEST['zip']);
+$db->bind(':teacher', $_REQUEST['teacher']);
 $db->bind(':email', $_REQUEST['email']);
 $db->bind(':grade', $_REQUEST['grade']);
-$db->bind(':size', $_REQUEST['size']);
 $db->bind(':attend', $_REQUEST['attend']);
 $db->bind(':troop', $_REQUEST['troop']);
+$db->bind(':size', $_REQUEST['size']);
 $db->bind(':school', $_REQUEST['school']);
-$db->bind(':teacher', $_REQUEST['teacher']);
 $db->bind(':topic1', $_REQUEST['topic1']);
 $db->bind(':topic2', $_REQUEST['topic2']);
 
